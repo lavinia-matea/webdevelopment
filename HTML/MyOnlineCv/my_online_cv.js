d21bf91847a8cfe6d91264ec0946b01e59803cb3
“Lavinia <lavinia.matea@gmail.com>
2023-01-21 08:22:50 +0100

    proj

---
+++ HTML/MyOnlineCv/my_online_cv.js
@@ -0,0 +1,57 @@
+// Music Player
+ let playBtn = document.getElementById("playBtn");
+
+
+let wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#ddd',
+    progressColor: '#ff006c',
+    barWidth: 3,
+    responsive: true,
+    height: 90,
+    barRadius: 4
+
+});
+
+wavesurfer.load('music_player/StoryOfMyLife.mp3');
+
+playBtn.onclick = function (){
+    wavesurfer.playPause();
+    if (playBtn.src.includes("play.png")) {
+        playBtn.src = "music_player/pause.png";
+    }else{
+        playBtn.src = "music_player/play.png";
+
+    }
+}
+
+wavesurfer.on('finish', function() {
+    playBtn.src = "music_player/play.png";
+    wavesurfer.stop();
+})
+
+
+
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+// Dark Mode
+
+let darkmode= document.querySelector('#darkmode');
+
+darkmode.onclick = () => {
+    if (darkmode.classList.contains('bx-moon')) {
+        darkmode.classList.replace('bx-moon', 'bx-sun');
+        document.body.classList.add('active');
+    }
+    else {
+        darkmode.classList.replace('bx-sun', 'bx-moon');
+        document.body.classList.remove('active');
+}
+}
+
